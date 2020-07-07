6f8c84e5047df9645f304e88964ed94beee1244e
Jane <jane.e.nath@gmail.com>
2020-07-07 08:23:00 -0700

    create about page

--- debdeep_nath/src/pages/about.js
+++ debdeep_nath/src/pages/about.js
@@ -1,14 +1,35 @@
 import React from "react"
 import { Link } from "gatsby"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const About = () => (
+const About = ({data}) => (
   <Layout>
     <SEO title="about" />
-    <h1>about</h1>
+    <div className="layoutContainer">
+
+      <Img className="headshot" fluid={data.headshotImage.childImageSharp.fluid} />
+      <div className="textContainer">
+        <h2>About Debdeep</h2>
+        <p className="bio">Debdeep Nath is a Pediatric Nurse Practitioner in Beaverton, OR, specializing in  diagnosis, assessment and medical management of ASD and common co-occurring physical and mental health conditions. Originally from India, Nath moved to the east coast to pursue his academic and professional training at Yale University. He enjoys exploring the beautiful outdoors of the Pacific Northwest and spending time with his wife and son. Nath joined the board of directors in 2019.</p>
+      </div>
+    </div>
   </Layout>
 )
+
+export const query = graphql`
+query aboutQuery {
+  headshotImage: file(relativePath: { eq: "debdeep_headshot.jpg" }) {
+    childImageSharp {
+      fluid(maxWidth: 1000) {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+}
+`
 
 export default About
--- debdeep_nath/src/pages/index.js
+++ debdeep_nath/src/pages/index.js
@@ -1,21 +1,35 @@
 import React from "react"
 import { Link } from "gatsby"
+import Img from "gatsby-image"
+
+import { graphql } from 'gatsby'
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" />
-    <div className="homeContainer">
-      <Image />
-      <div className="homeTextContainer">
-        <p>Diagnosis, assessment, and medical management for pediatric mental health concerns such as depression, anxiety, autism spectrum disorders, and ADD/ADHD.</p>
+    <div className="layoutContainer">
+      <Img className="homeImage" fluid={data.homeImage.childImageSharp.fluid} />
+      <div className="textContainer">
+        <p className="homeText">Diagnosis, assessment, and medical management for pediatric mental health concerns such as depression, anxiety, autism spectrum disorders, and ADD/ADHD.</p>
         <button><Link to="/about">Learn More</Link></button>
       </div>
     </div>
   </Layout>
 )
+
+export const query = graphql`
+query homeQuery {
+  homeImage: file(relativePath: { eq: "father_son.png" }) {
+    childImageSharp {
+      fluid(maxWidth: 1000) {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+}
+`
 
 export default IndexPage
